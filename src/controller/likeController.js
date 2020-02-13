693feaa7da1d820cfd77ec3ba63f3a5bcce5a22b
lucasquintino <lucasqf_2@hotmail.com>
2020-02-12 22:51:57 -0300

    'commit'

---
+++ src/controller/likeController.js
@@ -0,0 +1,32 @@
+const Dev = require("../models/Dev");
+
+module.exports = {
+  async store(req, res) {
+    console.log(req.io)
+    console.log(req.connectedUsers)
+    const {user} = req.headers
+    const {devId} = req.params 
+
+    const loggedDev = await Dev.findById(user)
+    const targetDev = await Dev.findById(devId)
+
+    if(!targetDev) {
+        return res.status(400).json('Dev not exists')
+    }
+
+    if(targetDev.likes.includes(loggedDev._id)){
+      const loggedSocket = req.connectedUsers[user]
+      const targetSocket = req.connectedUsers[devId]
+      if(loggedSocket)
+        req.io.to(loggedSocket).emit('match', targetDev)
+
+      if(targetSocket)
+        req.io.to(targetSocket).emit('match', loggedDev)
+    }
+    
+    loggedDev.likes.push(targetDev._id)
+    await loggedDev.save()
+
+    return res.json(loggedDev)
+  }
+};
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const devController = require("./controller/devController");
+const likeController = require("./controller/likeController");
+const dislikeController = require("./controller/dislikeController");
+const routes = express.Router();
+
+
+routes.get("/devs", devController.index);
+routes.post("/devs", devController.store);
+routes.post("/devs/:devId/likes", likeController.store);
+routes.post("/devs/:devId/dislikes", dislikeController.store);
+
+module.exports = routes;
